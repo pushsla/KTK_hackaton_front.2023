711091a1d75753ef47cf57d1a01e9d3fe5f367ba
Svyatoslav Pushkarev <push2001sla@gmail.com>
2023-11-14 09:09:12 +0500

    space in name fix

--- apps/landonprism/src/routes/api/stats/+page.server.ts
+++ apps/landonprism/src/routes/api/stats/+page.server.ts
@@ -1,10 +1,12 @@
 import { prism } from "$lib/prisma";
 import type { PageServerLoad } from "../../$types";
 
-export const load = async () => {
+export const load = (async () => {
     const response = await prism.cms_stats.findMany({
         orderBy: {
             id: "desc"
         }
     });
-};
+
+    return { stat: response[0] };
+}) satisfies PageServerLoad;
